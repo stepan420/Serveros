b0a16fa21c22c59bef64ed670c3fbde729a9bb77
tupy <tupy@porg.cz>
2019-09-23 17:22:38 +0200

    Server

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const http = require("http")
+
+let citac = 0
+
 http.createServer((req, res) => {
+    if (req.url == "/"){citac++}
     res.writeHead(200, {"Content-type":"text/html"})
-    res.end("<html lang='cs'><head><meta charset='UTF8'></head><body>SKRRRÁ</body></html>")
+    res.end("<html lang='cs'><head><meta charset='UTF8'></head><body>Počet volání: "+citac+"</body></html>")
 }).listen(88)
