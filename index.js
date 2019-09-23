5d3900d956e01bc58d5147c141d4d0ada099090c
tupy <tupy@porg.cz>
2019-09-23 17:40:34 +0200

    Serveros

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const http = require("http")
+const http = require("http");
 
-let citac = 0
+let citac = 0;
 
 http.createServer((req, res) => {
     if (req.url == "/"){citac++}
-    res.writeHead(200, {"Content-type":"text/html"})
-    res.end("<html lang='cs'><head><meta charset='UTF8'></head><body>Počet volání: "+citac+"</body></html>")
-}).listen(88)
+    if (req.url == "/blipblop"){
+        res.writeHead(200, {"Content-type":"text/html"});
+        res.end("<html lang='cs'><head><meta charset='UTF8'></head><body>Flip Flop</body></html>")
+    }
+    else if (req.url == "/jsondemoperson"){
+        res.writeHead(200, {"Content-type":"application/json"});
+        let obj = {};
+        obj.jmeno = "Bob";
+        obj.prijmeni = "Bobíček";
+        obj.rokNarozeni = 2069;
+        res.end(JSON.stringify(obj))
+    }
+    else{
+        res.writeHead(200, {"Content-type":"text/html"});
+        res.end("<html lang='cs'><head><meta charset='UTF8'></head><body>Počet volání: "+citac+"</body></html>")
+    }
+}).listen(88);
